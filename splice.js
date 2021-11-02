937ab46ba493445612c500e746c04b6bd25ec997
Jerika Kanz <jerikacapila@gmail.com>
2021-11-02 15:01:05 +0100

    Add Items Using splice()

--- splice.js
+++ splice.js
@@ -12,3 +12,25 @@ const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.splice(4, 4);
 arr.shift();
 console.log(arr);
+
+/* The third parameter of .splice() can be used to add one or more items to the array, which will be inserted starting at that same index. */
+
+const numbers = [10, 11, 12, 12, 15];
+numbers.splice(3, 1, 13, 14);
+console.log(numbers);
+
+// Challenge
+
+function htmlColorNames(arr) {
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  return arr;
+}
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
