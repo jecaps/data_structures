c115eac2375458819fe444e9ede618b9d48a49b1
Jerika Kanz <jerikacapila@gmail.com>
2021-11-02 14:20:23 +0100

    Access an Array's Contents Using Bracket Notation

---
+++ bracket_notation.js
@@ -0,0 +1,13 @@
+// Use bracket notation to access an element of an array.
+
+let ourArray = ["a", "b", "c"];
+let ourVariable = ourArray[0];
+ourArray[1] = "not b anymore";
+
+console.log(ourArray);
+
+// Challenge
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "beautiful";
+console.log(myArray);
