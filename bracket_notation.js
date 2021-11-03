829ecd6eb61393da8a094fda354a641869fb35cc
Jerika Kanz <jerikacapila@gmail.com>
2021-11-03 16:43:27 +0100

    Access Property Names with Bracket Notation

--- bracket_notation.js
+++ bracket_notation.js
@@ -11,3 +11,20 @@ console.log(ourArray);
 let myArray = ["a", "b", "c", "d"];
 myArray[1] = "beautiful";
 console.log(myArray);
+
+/* We can also use bracket notation to access properties(keys) within an object.
+  Bracket notation is very useful  because sometimes object properties are not known before runtime or we need to access them in a more dynamic way.*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+}
+console.log(checkInventory("apples"));
